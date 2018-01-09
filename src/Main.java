45c96923d6989a144afeefa86712d6d3d97afd87
Szczepcio <szczepan.szczepan2@gmail.com>
2018-01-09 21:43:35 +0100

    h

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
+import Shell.Shell;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+        
 
 	}
 
